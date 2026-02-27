e8ad345b66df2d7de0549fa0146f5c71bfd7b0ab
orkvacy <krotago@gmail.com>
2026-02-27 21:21:19 +0800

    add posttest 1

---
+++ POSTTEST_1/posttest1/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtIPS = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Histori = New System.Windows.Forms.ListBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtIPS
+        '
+        Me.txtIPS.Location = New System.Drawing.Point(273, 394)
+        Me.txtIPS.Name = "txtIPS"
+        Me.txtIPS.Size = New System.Drawing.Size(427, 31)
+        Me.txtIPS.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(707, 392)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(102, 40)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "+ Tambah"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(273, 556)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(516, 43)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(393, 204)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 79)
+        Me.Label1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(415, 308)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 25)
+        Me.Label2.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(267, 370)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 25)
+        Me.Label3.TabIndex = 5
+        '
+        'Histori
+        '
+        Me.Histori.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Histori.FormattingEnabled = True
+        Me.Histori.ItemHeight = 25
+        Me.Histori.Location = New System.Drawing.Point(273, 459)
+        Me.Histori.Name = "Histori"
+        Me.Histori.Size = New System.Drawing.Size(516, 75)
+        Me.Histori.TabIndex = 6
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(996, 100)
+        Me.Panel1.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Britannic Bold", 16.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(339, 28)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(300, 48)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Kalkulator IPK"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.DimGray
+        Me.Label5.Location = New System.Drawing.Point(337, 147)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(342, 37)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "TOTAL IPK SAAT INI"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(996, 663)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Histori)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtIPS)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtIPS As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Histori As ListBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ POSTTEST_1/posttest1/Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Xml
+
+Public Class Form1
+    Private daftarIPS As New List(Of Double)()
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ipsBaru As Double
+
+        Label3.Text = ""
+        'paksa ubah keyboard jadi eng
+        If Double.TryParse(txtIPS.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, ipsBaru) Then
+
+            If ipsBaru >= 0 AndAlso ipsBaru <= 4.0 Then
+                daftarIPS.Add(ipsBaru)
+                Histori.Items.Add("Semester " & daftarIPS.Count & " : " & ipsBaru.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture))
+
+                KalkulasiIPK()
+
+                txtIPS.Clear()
+                txtIPS.Focus()
+            Else
+                Label3.Text = "Rentang IP hanya 0-4!"
+                Label3.ForeColor = Color.Red
+            End If
+        Else
+            Label3.Text = "Input harus berupa angka"
+            Label3.ForeColor = Color.Red
+        End If
+    End Sub
+
+    ' --- Tombol Reset ---
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        daftarIPS.Clear()
+        Histori.Items.Clear()
+        Label1.Text = "-"
+        Label2.Text = "-"
+        Label3.Text = ""
+        txtIPS.Clear()
+        txtIPS.Focus()
+    End Sub
+
+    Private Sub KalkulasiIPK()
+        If daftarIPS.Count > 0 Then
+            Dim ipk As Double = daftarIPS.Average()
+
+            Label1.Text = ipk.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture)
+
+            If ipk >= 3.01 Then
+                Label2.Text = "Sangat Memuaskan"
+            ElseIf ipk >= 2.76 Then
+                Label2.Text = "Memuaskan"
+            ElseIf ipk >= 2.0 Then
+                Label2.Text = "Cukup"
+            Else
+                Label2.Text = "Kurang"
+            End If
+        End If
+    End Sub
+
+    'error handling
+    Private Sub txtIPS_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtIPS.KeyPress
+        'error handling cuma bisa input angka, titik, dan backspace
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) AndAlso e.KeyChar <> "."c Then
+            e.Handled = True
+            Label3.Text = "Gunakan titik '.' untuk penggunaan koma"
+            Label3.ForeColor = Color.Red
+        Else
+            Label3.Text = ""
+        End If
+
+        'titik cuma bisa sekali
+        If e.KeyChar = "."c AndAlso txtIPS.Text.Contains(".") Then
+            e.Handled = True
+        End If
+    End Sub
+
+
+    Private Sub cekRiwayat(sender As Object, e As EventArgs) Handles Histori.SelectedIndexChanged
+    End Sub
+
+End Class
