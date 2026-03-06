99578a8a864741d3c6eb09c0a71b123f533d24a2
orkvacy <krotago@gmail.com>
2026-03-06 21:11:20 +0800

    add : functional only

---
+++ POSTTEST_2/posttest2/posttest2/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtJudul = New System.Windows.Forms.TextBox()
+        Me.txtGenre = New System.Windows.Forms.TextBox()
+        Me.txtDelBuku = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnDel = New System.Windows.Forms.Button()
+        Me.lblText = New System.Windows.Forms.Label()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtJudul
+        '
+        Me.txtJudul.Location = New System.Drawing.Point(300, 162)
+        Me.txtJudul.Name = "txtJudul"
+        Me.txtJudul.Size = New System.Drawing.Size(100, 31)
+        Me.txtJudul.TabIndex = 0
+        '
+        'txtGenre
+        '
+        Me.txtGenre.Location = New System.Drawing.Point(300, 239)
+        Me.txtGenre.Name = "txtGenre"
+        Me.txtGenre.Size = New System.Drawing.Size(100, 31)
+        Me.txtGenre.TabIndex = 1
+        '
+        'txtDelBuku
+        '
+        Me.txtDelBuku.Location = New System.Drawing.Point(1050, 162)
+        Me.txtDelBuku.Name = "txtDelBuku"
+        Me.txtDelBuku.Size = New System.Drawing.Size(100, 31)
+        Me.txtDelBuku.TabIndex = 2
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(350, 313)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(109, 51)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "Button1"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
+        Me.DataGridView1.Location = New System.Drawing.Point(102, 480)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 82
+        Me.DataGridView1.RowTemplate.Height = 33
+        Me.DataGridView1.Size = New System.Drawing.Size(1055, 168)
+        Me.DataGridView1.TabIndex = 5
+        '
+        'btnDel
+        '
+        Me.btnDel.Location = New System.Drawing.Point(1092, 322)
+        Me.btnDel.Name = "btnDel"
+        Me.btnDel.Size = New System.Drawing.Size(147, 72)
+        Me.btnDel.TabIndex = 4
+        Me.btnDel.Text = "Button1"
+        Me.btnDel.UseVisualStyleBackColor = True
+        '
+        'lblText
+        '
+        Me.lblText.AutoSize = True
+        Me.lblText.Location = New System.Drawing.Point(549, 432)
+        Me.lblText.Name = "lblText"
+        Me.lblText.Size = New System.Drawing.Size(77, 25)
+        Me.lblText.TabIndex = 6
+        Me.lblText.Text = "Label1"
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Judul Buku"
+        Me.Column1.MinimumWidth = 10
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 200
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Genre"
+        Me.Column2.MinimumWidth = 10
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 200
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1524, 732)
+        Me.Controls.Add(Me.lblText)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnDel)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtDelBuku)
+        Me.Controls.Add(Me.txtGenre)
+        Me.Controls.Add(Me.txtJudul)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtJudul As TextBox
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents txtDelBuku As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnDel As Button
+    Friend WithEvents lblText As Label
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+End Class
---
+++ POSTTEST_2/posttest2/posttest2/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+
+    Private Sub refresh()
+        DataGridView1.Rows.Clear()
+        For i As Integer = 0 To totalBuku - 1
+            DataGridView1.Rows.Add(dataBuku(i).Judul, dataBuku(i).Genre)
+        Next
+    End Sub
+
+
+
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim judulBaru As String = txtJudul.Text
+        Dim genreBaru As String = txtGenre.Text
+
+        If judulBaru = "" Or genreBaru = "" Then
+            lblText.ForeColor = Color.Red
+            lblText.Text = "Judul dan Genre harus terisi dua-duanya"
+            Return
+        End If
+
+        If CariIndeksBuku(judulBaru) <> -1 Then
+            lblText.ForeColor = Color.Red
+            lblText.Text = "Buku ini sudah " & judulBaru & "' sudah ada"
+            txtJudul.Clear()
+            txtJudul.Focus()
+            Return
+        End If
+
+        addBuku(judulBaru, genreBaru)
+        refresh()
+
+
+        lblText.ForeColor = Color.Green
+        lblText.Text = "Done, buku berhasil tertambah"
+
+        txtJudul.Clear()
+        txtGenre.Clear()
+        txtJudul.Focus()
+    End Sub
+
+    Private Sub btnDel_Click(sender As Object, e As EventArgs) Handles btnDel.Click
+        Dim judulYangDihapus As String = txtDelBuku.Text
+
+        If judulYangDihapus = "" Then
+            lblText.ForeColor = Color.Red
+            lblText.Text = "Silahkan Input sesuatu"
+            Return
+        End If
+
+
+        Dim indeks As Integer = CariIndeksBuku(judulYangDihapus)
+
+        If indeks <> -1 Then
+            delBuku(indeks)
+            refresh()
+            txtDelBuku.Clear()
+
+            lblText.ForeColor = Color.Green
+            lblText.Text = "Done'" & judulYangDihapus & " berhasil dihapus!"
+        Else
+            lblText.ForeColor = Color.Red
+            lblText.Text = "Buku tersebut tidak ditemukan"
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
